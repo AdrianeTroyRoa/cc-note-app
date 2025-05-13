ace47a989ff4053b29d081cf98bd39fdf5c639f4
Troy Roa <roatroy850@gmail.com>
2025-05-14 00:26:43 +0800

    fix: apostrophe in login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -62,7 +62,7 @@ export default function LoginPage() {
           </div>
 
           <p className="text-center text-sm text-gray-500">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <a href="/register" className="text-blue-600 hover:underline">
               Register
             </a>
